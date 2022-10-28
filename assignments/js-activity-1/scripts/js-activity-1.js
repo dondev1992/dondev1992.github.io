2de82c3131c8d89c28c351e0c3ad5cf7308c426e
Don Moore <79601799+dondev1992@users.noreply.github.com>
2022-10-28 01:12:43 -0400

    added customer data

---
+++ assignments/js-activity-1/scripts/js-activity-1.js
@@ -0,0 +1,37 @@
+const customers = [
+  {
+    customer_id: 001,
+    last_name: "Smith",
+    first_name: "Jacob",
+    last_purchase_quantity: 5,
+    last_purchase_amount_in_dollars: $50,
+  },
+  {
+    customer_id: 002,
+    last_name: "Johnson",
+    first_name: "Iris",
+    last_purchase_quantity: 10,
+    last_purchase_amount_in_dollars: $100,
+  },
+  {
+    customer_id: 003,
+    last_name: "Perez",
+    first_name: "Javier",
+    last_purchase_quantity: 5,
+    last_purchase_amount_in_dollars: $50,
+  },
+  {
+    customer_id: 004,
+    last_name: "Thomas",
+    first_name: "Henry",
+    last_purchase_quantity: 20,
+    last_purchase_amount_in_dollars: $200,
+  },
+  {
+    customer_id: 005,
+    last_name: "Smith",
+    first_name: "Janice",
+    last_purchase_quantity: 10,
+    last_purchase_amount_in_dollars: $100,
+  },
+];
