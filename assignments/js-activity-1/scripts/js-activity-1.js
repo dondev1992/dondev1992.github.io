63b159da41b0666c5cdc702acae5cec762f91a29
Don Moore <79601799+dondev1992@users.noreply.github.com>
2022-10-31 14:36:27 -0400

    changed customer ids to strings to get desired putput

--- assignments/js-activity-1/scripts/js-activity-1.js
+++ assignments/js-activity-1/scripts/js-activity-1.js
@@ -1,34 +1,34 @@
 const customers = [
   {
-    customer_id: 001,
+    customer_id: "001",
     last_name: "Smith",
     first_name: "Jacob",
     last_purchase_quantity: 5,
     last_purchase_amount_in_dollars: 50,
   },
   {
-    customer_id: 002,
+    customer_id: "002",
     last_name: "Johnson",
     first_name: "Iris",
     last_purchase_quantity: 10,
     last_purchase_amount_in_dollars: 100,
   },
   {
-    customer_id: 003,
+    customer_id: "003",
     last_name: "Perez",
     first_name: "Javier",
     last_purchase_quantity: 5,
     last_purchase_amount_in_dollars: 50,
   },
   {
-    customer_id: 004,
+    customer_id: "004",
     last_name: "Thomas",
     first_name: "Henry",
     last_purchase_quantity: 20,
     last_purchase_amount_in_dollars: 200,
   },
   {
-    customer_id: 005,
+    customer_id: "005",
     last_name: "Smith",
     first_name: "Janice",
     last_purchase_quantity: 10,
@@ -42,23 +42,23 @@ const getCustomerData = () => {
   const customerId = parseInt(id);
   for (let i = 0; i < customers.length; i++) {
     if (customerId > 000 && customerId < 006) {
-      if (customerId === customers[i].customer_id) {
-        console.log(
-          `The customer with id #${customers[i].customer_id} is ${customers[i].first_name} ${customers[i].last_name}, whose last purchase was ${customers[i].last_purchase_quantity} Dynamic Monkey backpacks in the amount of $${customers[i].last_purchase_amount_in_dollars}.`
-        );
-
+      if (customerId === parseInt(customers[i].customer_id)) {
         document.getElementById(
           "customer-lookup-results"
-        ).innerHTML = `The customer with id #${customers[i].customer_id} is ${customers[i].first_name} ${customers[i].last_name}, whose last purchase was ${customers[i].last_purchase_quantity} Dynamic Monkey backpacks in the amount of $${customers[i].last_purchase_amount_in_dollars}.`;
+        ).innerHTML = `The customer with id #${customers[
+          i
+        ].customer_id.toString()} is ${customers[i].first_name} ${
+          customers[i].last_name
+        }, whose last purchase was ${
+          customers[i].last_purchase_quantity
+        } Dynamic Monkey backpacks in the amount of $${
+          customers[i].last_purchase_amount_in_dollars
+        }.`;
       }
     } else {
-      console.log(
-        `Customer id: ${id} doesn't exist. Enter a valid customer id.`
-      );
-      //document.getElementById("customer-lookup").reset();
       document.getElementById(
         "customer-lookup-results"
-      ).innerHTML = `Customer id: ${id} doesn't exist. Enter a valid customer id.`;
+      ).innerHTML = `Customer id: #${id} doesn't exist. Enter a valid customer id.`;
     }
   }
 };
