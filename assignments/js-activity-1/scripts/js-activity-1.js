f0726ca05e687bda1eb91772cf41fb60a370aa65
Don Moore <79601799+dondev1992@users.noreply.github.com>
2022-10-31 14:21:58 -0400

    added text output and form reset button to customer lookup

--- assignments/js-activity-1/scripts/js-activity-1.js
+++ assignments/js-activity-1/scripts/js-activity-1.js
@@ -35,9 +35,10 @@ const customers = [
     last_purchase_amount_in_dollars: 100,
   },
 ];
+const getIdButton = document.getElementById("get-id-button");
 
-function getCustomerData() {
-  const id = prompt("Enter customer id");
+const getCustomerData = () => {
+  let id = document.getElementById("customer-lookup").value;
   const customerId = parseInt(id);
   for (let i = 0; i < customers.length; i++) {
     if (customerId > 000 && customerId < 006) {
@@ -45,11 +46,25 @@ function getCustomerData() {
         console.log(
           `The customer with id #${customers[i].customer_id} is ${customers[i].first_name} ${customers[i].last_name}, whose last purchase was ${customers[i].last_purchase_quantity} Dynamic Monkey backpacks in the amount of $${customers[i].last_purchase_amount_in_dollars}.`
         );
+
+        document.getElementById(
+          "customer-lookup-results"
+        ).innerHTML = `The customer with id #${customers[i].customer_id} is ${customers[i].first_name} ${customers[i].last_name}, whose last purchase was ${customers[i].last_purchase_quantity} Dynamic Monkey backpacks in the amount of $${customers[i].last_purchase_amount_in_dollars}.`;
       }
     } else {
       console.log(
-        `Customer id: ${id} doesn"t exist. Enter a valid customer id.`
+        `Customer id: ${id} doesn't exist. Enter a valid customer id.`
       );
+      //document.getElementById("customer-lookup").reset();
+      document.getElementById(
+        "customer-lookup-results"
+      ).innerHTML = `Customer id: ${id} doesn't exist. Enter a valid customer id.`;
     }
   }
-}
+};
+
+const resetTextField = () => {
+  let id = document.getElementById("customer-lookup");
+  id.value = "";
+  document.getElementById("customer-lookup-results").innerHTML = "";
+};
