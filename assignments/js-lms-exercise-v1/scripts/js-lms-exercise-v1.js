6b6294f37242fb984160fd908b5888a9c5fb5b21
Don Moore <79601799+dondev1992@users.noreply.github.com>
2022-11-02 19:49:09 -0400

    completed js-lms-exercise

--- assignments/js-lms-exercise-v1/scripts/js-lms-exercise-v1.js
+++ assignments/js-lms-exercise-v1/scripts/js-lms-exercise-v1.js
@@ -90,3 +90,162 @@ const testData = [
     submissionDate: "2/20/2020",
   },
 ];
+//Helper Functions
+
+// reset function
+const reset = () => {
+  location.reload();
+};
+
+//average function
+
+const average = (list, quantity) => {
+  let sum = null;
+  for (const count of list) {
+    sum += count;
+  }
+  finalAverage = (sum / quantity).toFixed(1);
+  return finalAverage;
+};
+// generate list function
+
+const generateList = (arrayData, selector) => {
+  for (let i = 0; i < arrayData.length; i++) {
+    const listTitle = document.createElement("h3");
+    const unorderedList = document.createElement("ul");
+    const itemQuizName = document.createElement("li");
+    const itemQuizModule = document.createElement("li");
+    const itemQuizScore = document.createElement("li");
+    const itemStudentId = document.createElement("li");
+    const itemStudentName = document.createElement("li");
+    const itemSubmissionDate = document.createElement("li");
+
+    listTitle.innerText = `Submission #${arrayData.indexOf(arrayData[i]) + 1}`;
+    itemQuizName.innerText = `Quiz Name: ${arrayData[i].quizName}`;
+    itemQuizModule.innerText = `Quiz Module: ${arrayData[i].quizModule}`;
+    itemQuizScore.innerText = `Quiz Score: ${arrayData[i].quizScore}`;
+    itemStudentId.innerText = `Student Id: ${arrayData[i].studentId}`;
+    itemStudentName.innerText = `Student Name: ${arrayData[i].studentName}`;
+    itemSubmissionDate.innerText = `Submission Date: ${arrayData[i].submissionDate}`;
+
+    unorderedList.appendChild(itemQuizName);
+    unorderedList.appendChild(itemQuizModule);
+    unorderedList.appendChild(itemQuizScore);
+    unorderedList.appendChild(itemStudentId);
+    unorderedList.appendChild(itemStudentName);
+    unorderedList.appendChild(itemSubmissionDate);
+
+    document.querySelector(`#${selector}`).appendChild(listTitle);
+    document.querySelector(`#${selector}`).appendChild(unorderedList);
+  }
+};
+// Filter By Date Feature
+
+const filterByDate = (event) => {
+  let input = event.target.parentNode.querySelector("input");
+  let date = input.value;
+  const selectorValue = "date-results";
+  console.log(date);
+  const submissions = [];
+  if (testData.length < 1) {
+    console.log(submissions);
+  } else {
+    for (const submission of testData) {
+      if (date === submission.submissionDate) {
+        submissions.push(submission);
+      }
+    }
+    console.log(submissions);
+  }
+  input.value = "";
+  generateList(submissions, selectorValue);
+};
+
+document.getElementById("date-button").addEventListener("click", filterByDate);
+
+// filterByDate("1/20/2021", emptyArray);
+
+// Filter By StudentId Feature
+
+const filterByStudentId = (event) => {
+  let input = event.target.parentNode.querySelector("#id");
+  let studentId = parseInt(input.value);
+  const selectorValue = "id-results";
+
+  const students = [];
+
+  if (testData.length < 1) {
+    console.log(students);
+  } else {
+    for (const student of testData) {
+      if (studentId === student.studentId) {
+        students.push(student);
+      }
+    }
+    console.log(students);
+  }
+  generateList(students, selectorValue);
+  input.value = "";
+};
+
+document
+  .getElementById("id-button")
+  .addEventListener("click", filterByStudentId);
+
+// filterByStudentId(15, testData);
+
+// Find Unsubmitted Feature
+
+const studentNames = ["Diana Ross", "Steve Smith", "Bob Ross", "Dylan Roberts"];
+
+const findUnsubmitted = (event) => {
+  let input = event.target.parentNode.querySelector("#date-submitted");
+  let date = input.value;
+  let names = [...studentName];
+  console.log(date);
+  if (testData.length < 1) {
+    document.querySelector("#unsubmitted-results").textContent = "Error";
+  } else {
+    for (const submission of testData) {
+      if (date === submission.submissionDate) {
+        let name = submission.names;
+        for (let i = 0; i < studentNames.length; i++) {
+          if (name === names[i]) {
+            names.splice(i, 1);
+          }
+        }
+      }
+    }
+  }
+  document.querySelector(
+    "#unsubmitted-results"
+  ).textContent = `Students that haven't submitted assignmants on this date ${date} are: ${studentNames}`;
+  input.value = "";
+};
+
+document
+  .getElementById("unsubmitted-button")
+  .addEventListener("click", findUnsubmitted);
+// findUnsubmitted("12/20/2020", studentNames, testData);
+
+// Get Quiz Average Feature
+
+const getAverageScore = () => {
+  totalScore = [];
+  if (testData.length > 0) {
+    for (const submission of testData) {
+      totalScore.push(submission.quizScore);
+    }
+    average(totalScore, totalScore.length);
+  } else {
+    console.log(totalScore);
+  }
+  document.querySelector(
+    "#quiz-average"
+  ).textContent = `Quiz Average: ${finalAverage}`;
+};
+document
+  .getElementById("quiz-average-button")
+  .addEventListener("click", getAverageScore);
+
+// getAverageScore(testData);
